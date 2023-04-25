bc0747076aa66ebd0a7dcb5a0ff1bfcad988fb25
Mahfoozalam1516 <98471514+Mahfoozalam1516@users.noreply.github.com>
2023-04-25 21:28:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "616eb3c48amsh8619dd255502b89p1c9872jsnb5cab5705c5b",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "5b74acd104dba68d6df2ee9ec6830b87"; // enter your key from openweather API
